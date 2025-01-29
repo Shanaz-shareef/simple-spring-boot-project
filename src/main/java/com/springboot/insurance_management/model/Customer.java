148331db1aa204a621541e1c066412d1d111262e
shanaz <daizyshan@gmail.com>
2025-01-29 13:12:57 -0500

    created model classes for Customer, Claim and Policy

---
+++ src/main/java/com/springboot/insurance_management/model/Customer.java
@@ -0,0 +1,46 @@
+package com.springboot.insurance_management.model;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.engine.internal.Cascade;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name="customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( name="customer_id")
+    private int customerId;
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false, unique = true, name="user_name")
+    private String userName;
+
+    @Column(nullable=false, unique = true)
+    private String email;
+      @Column(name="phone_number")
+    private int phoneNumber;
+    @Column(nullable = false, name="date_of_birth")
+    private LocalDate dateOfBirth;
+    @ManyToMany(cascade=CascadeType.ALL)
+    @JoinTable( joinColumns=@JoinColumn(name="customer_id"),
+            inverseJoinColumns =@JoinColumn(name="policy_id")
+
+    )
+    private Set<Policy> policies;
+    @OneToMany(mappedBy = "customers")
+    private List<Claim>claims;
+}
---
+++ src/main/java/com/springboot/insurance_management/model/Policy.java
@@ -0,0 +1,40 @@
+package com.springboot.insurance_management.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="policy")
+public class Policy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="policy_id")
+    private int policyId;
+    @Column(name="policy_name")
+    private String policyName;
+    @Column(name="policy_type")
+    private double policyType;
+    @Column(name="premium_amount")
+    private double premiumAmount;
+    @Column(name="coverage_amount")
+    private double coverageAmount;
+    @Column(name="start_date")
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+    @ManyToMany(mappedBy = "policies")
+    private Set<Customer>customer;
+    @OneToMany(mappedBy="policy", cascade = CascadeType.ALL)
+    private Set<Claim>claims;
+
+}
